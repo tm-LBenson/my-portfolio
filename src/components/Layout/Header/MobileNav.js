c1d1f1653304b8eb67221152ee7938eedc2e8f9b
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-16 18:05:20 -0400

    stop body from scrolling when drawer is open

--- src/components/Layout/Header/MobileNav.js
+++ src/components/Layout/Header/MobileNav.js
@@ -1,11 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Divide as Hamburger } from 'hamburger-react';
 
 function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      const drawer = document.querySelector('.mobile-nav');
+      const closeButton = document.querySelector('.hamburger-react');
+
+      if (
+        !drawer.contains(e.target) &&
+        !closeButton.contains(e.target) &&
+        isOpen
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    if (!isOpen) {
+      document.body.classList.add('drawer-open');
+    } else {
+      document.body.classList.remove('drawer-open');
+    }
   };
 
   return (

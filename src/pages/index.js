70efd731f604ac78d426b883c5aed2d235274c06
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-17 01:05:10 -0400

    add projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import About from '@src/components/About';
 import Hero from '@src/components/Hero';
 import Layout from '@src/components/Layout';
+import Projects from '@src/components/Projects';
+import TechnicalSkills from '@src/components/Skills';
 
 function HomePage() {
   return (
     <Layout>
       <Hero />
       <About />
+      <TechnicalSkills />
+      <Projects />
     </Layout>
   );
 }

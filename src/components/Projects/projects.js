82f5442b3ba06980bf99b84bf0d760a90b3449e7
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-17 13:27:49 -0400

    put projects into separate file

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    name: 'Tune Port',
+    shortDescription:
+      'Application for Spotify premium users officially approved by Spotify for production allows unlimited users.',
+    description:
+      'Built with React on Vite, using Mantine, SASS, Express, OAuth, and the Spotify API.',
+    imageUrl: '/site4.png',
+    websiteUrl: 'https://tune-port.netlify.app/',
+    githubUrl: 'https://github.com/tm-LBenson/tune-port',
+    tags: ['React', 'Express', 'OAuth', 'Spotify API'],
+  },
+  {
+    name: 'Prodigy Path',
+    shortDescription:
+      'Social media application for mentors and mentees to connect and work together.',
+    description:
+      'Built with Node.js, Socket.io, React.js, MongoDB, and Express, which includes role-based access control for added security.',
+    imageUrl: '/site2.png',
+    websiteUrl: 'https://prodigy-path.netlify.app/',
+    githubUrl: 'https://github.com/tm-LBenson/prodigy-path',
+    tags: ['Node.js', 'React', 'MongoDB', 'Express', 'Socket.io'],
+  },
+  {
+    name: 'YelpCamp',
+    shortDescription:
+      'Full-stack application used for reviewing and sharing campgrounds around the world.',
+    description:
+      'Built with Node.Js, Express, EJS, CSS, Bootstrap, MongoDB, Server Side Rendering, Session Cookies, and Cloudinary.',
+    imageUrl: '/site3.png',
+    websiteUrl: 'https://yelp-camp-lewis.cyclic.app/',
+    githubUrl: 'https://github.com/tm-LBenson/yelp-camp-lewis',
+    tags: ['Node.js', 'Express', 'MongoDB', 'Bootstrap', 'Cloudinary'],
+  },
+];
+export default projects;

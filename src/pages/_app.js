362314c214b60345f43600f8ea7421fb65186de6
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-18 11:15:20 -0400

    move prism to public folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from '@src/components/ThemeContext';
 import '@src/styles/sass/main.scss';
 import Script from 'next/script';
-import prism from './prism';
+import prism from '../../public/prism';
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Script src="/prism.js" />
+
       <Component {...pageProps} />
-      <Script src={prism} />
     </ThemeProvider>
   );
 }

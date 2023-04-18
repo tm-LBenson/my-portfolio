4cf760e1405d3850c224aaa1357f1376e5bdd8fd
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-18 11:17:10 -0400

    remove unused variables

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import { ThemeProvider } from '@src/components/ThemeContext';
 import '@src/styles/sass/main.scss';
 import Script from 'next/script';
-import prism from '../../public/prism';
-import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>

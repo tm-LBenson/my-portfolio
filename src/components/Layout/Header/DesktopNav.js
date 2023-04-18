1a62e69fafe49d6619ba537550f7be52238e3e2c
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-18 10:53:49 -0400

    change a tag to Link component

--- src/components/Layout/Header/DesktopNav.js
+++ src/components/Layout/Header/DesktopNav.js
@@ -2,6 +2,7 @@
 import { useRouter } from 'next/router';
 import links from '@src/components/links';
 import { smoothScroll } from '@src/components/smoothScroll';
+import Link from 'next/link';
 
 function DesktopNav() {
   const router = useRouter();
@@ -13,13 +14,13 @@ function DesktopNav() {
             key={index}
             className="nav-item"
           >
-            <a
+            <Link
               href={link.href}
               className={link.cta ? 'cta-button nav-link' : 'nav-link'}
               onClick={(e) => smoothScroll(e, router)}
             >
               {link.text}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
